97686d10704f66d902aaeaabcb97d0f0d0d22ca2
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-16 10:14:59 -0300

    Added react-router to manage routes and top menu bar with saj logo.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+} from 'react-router-dom'
+import styled from 'styled-components'
 
-function App() {
+import Responsibles from './pages/responsibles'
+
+const StyledMenuBar = styled.nav`
+  display: flex;
+  background-color: #07bcdf;
+`
+
+const StyledLogo = styled.img`
+  margin: 0 1rem;
+`
+
+const StyledMenu = styled.ul`
+  flex: 1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  font-family: 'Roboto', sans-serif;
+  list-style: none;
+`
+
+const StyledMenuItem = styled.li`
+  margin: 0 1rem;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: ${({ active }) => (active ? '#fff' : '#2e2e2e')};
+
+  &:hover {
+    color: #fff;
+  }
+`
+
+const MenuLink = ({ label, to, exact }) => {
+  let match = useRouteMatch({
+    path: to,
+    exact: exact,
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StyledLink active={match} to={to}>
+      {label}
+    </StyledLink>
+  )
 }
 
-export default App;
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <StyledMenuBar>
+          <StyledLogo src="./logo.png" alt="" />
+          <StyledMenu>
+            <StyledMenuItem>
+              <MenuLink to="/responsibles" label="Responsáveis" />
+            </StyledMenuItem>
+          </StyledMenu>
+        </StyledMenuBar>
+
+        <Switch>
+          <Route path="/responsibles">
+            <Responsibles />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+function Home() {
+  return <h2>SAJ App</h2>
+}
+
+export default App
