84c037cd56eb9e3e7b161ea7bcc2872f83b3079a
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-17 20:08:49 -0300

    Added DefaulButton component.

---
+++ src/components/defaultButton.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledButton = styled.div`
+  font-family: 'Roboto', sans-serif;
+  font-size: 1rem;
+  background-color: #1fc5f0;
+  color: #fff;
+  padding: 0.8rem;
+  border-radius: 3px;
+  justify-self: center;
+  align-self: center;
+  transition: all 200ms;
+  user-select: none;
+  cursor: pointer;
+
+  &:hover {
+    transition: all 150ms linear;
+    opacity: 0.85;
+  }
+
+  &:active {
+    transition: all 150ms linear;
+    opacity: 0.75;
+  }
+`
+
+const DefaultButton = ({ children, ...props }) => (
+  <StyledButton {...props}>{children}</StyledButton>
+)
+
+export default DefaultButton
