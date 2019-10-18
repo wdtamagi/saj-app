be6c6c235c1ff38cdde9ff96e77c0845f76e857b
Willian Dias Tamagi <will.tamagi@gmail.com>
2019-10-17 23:54:34 -0300

    Added mask and date components to generically handle.

--- src/components/formInput.js
+++ src/components/formInput.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import InputMask from 'react-input-mask'
+import DatePicker from 'react-datepicker'
+
+import 'react-datepicker/dist/react-datepicker.css'
+import './formInput.css'
 
 const StyledInput = styled.input`
   display: block;
@@ -26,6 +31,50 @@ const StyledInput = styled.input`
   }
 `
 
+const StyledInputMask = styled(InputMask)`
+  display: block;
+  background: transparent;
+  outline: none;
+  width: 100%;
+  border: none;
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-bottom-color: #585858;
+  transition: 300ms all;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 100%;
+
+  &:focus {
+    border-bottom-color: #0187ff;
+  }
+`
+
+const StyledDatePicker = styled(DatePicker)`
+  display: block;
+  background: transparent;
+  outline: none;
+  width: 100%;
+  border: none;
+  border-bottom-width: 2px;
+  border-bottom-style: solid;
+  border-bottom-color: #585858;
+  transition: 300ms all;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 100%;
+
+  &:focus {
+    border-bottom-color: #0187ff;
+  }
+
+  &:focus + ${({ id }) => `#label_${id}`} {
+    color: #0187ff;
+    font-size: 75%;
+    transform: translate3d(0, -100%, 0);
+  }
+`
+
 const StyledLabel = styled.label`
   position: absolute;
   top: 0;
@@ -35,6 +84,7 @@ const StyledLabel = styled.label`
   font-family: 'Roboto', sans-serif;
   font-weight: 400;
   font-size: 90%;
+  pointer-events: none;
 
   ${({ value }) =>
     value &&
@@ -43,51 +93,70 @@ const StyledLabel = styled.label`
     font-size: 75%;
     transform: translate3d(0, -100%, 0);
   `}
+
+  ${({ focused }) =>
+    focused &&
+    `    color: #0187ff;
+    font-size: 75%;
+    transform: translate3d(0, -100%, 0);`}
 `
 
 const StyledWrapper = styled.div`
   position: relative;
 `
 
-export default class FormInput extends React.Component {
-  constructor(props) {
-    super(props)
-    this.textInput = React.createRef()
-    this.focusTextInput = this.focusTextInput.bind(this)
-  }
+const FormInput = ({
+  className,
+  type = 'text',
+  inputLabel,
+  value,
+  onChange,
+  id,
+  mask,
+}) => {
+  const [focused, setFocused] = useState(false)
 
-  componentDidMount() {
-    if (this.props.focusOnStart) {
-      this.textInput.current.focus()
-    }
-  }
-
-  focusTextInput() {
-    this.textInput.current.focus()
-  }
-
-  render() {
-    let {
-      className,
-      type = 'text',
-      inputLabel,
-      value,
-      onChange,
-      id,
-    } = this.props
-    return (
-      <StyledWrapper className={className}>
+  return (
+    <StyledWrapper className={className}>
+      {mask && (
+        <StyledInputMask
+          mask={mask}
+          guide={false}
+          id={id}
+          value={value}
+          onChange={onChange}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          maskPlaceholder=""
+        />
+      )}
+      {!mask && type !== 'date' && (
         <StyledInput
           type={type}
           id={id}
           value={value}
           onChange={onChange}
-          ref={this.textInput}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
         />
-        <StyledLabel value={value} onClick={this.focusTextInput}>
-          {inputLabel}
-        </StyledLabel>
-      </StyledWrapper>
-    )
-  }
+      )}
+      {!mask && type === 'date' && (
+        <StyledDatePicker
+          dateFormat="dd/MM/yyyy"
+          id={id}
+          selected={value}
+          onChange={onChange}
+          onFocus={() => setFocused(true)}
+          onBlur={() => setFocused(false)}
+          filterDate={date => date < new Date()}
+        />
+      )}
+
+      <StyledLabel id={`label_${id}`} value={value} focused={focused}>
+        {inputLabel}
+      </StyledLabel>
+    </StyledWrapper>
+  )
 }
+
+export default FormInput
